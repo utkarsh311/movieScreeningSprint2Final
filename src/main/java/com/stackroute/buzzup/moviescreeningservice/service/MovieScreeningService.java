ce8742f40d1dfe08c813c7e3c4612aa81fa6cdaf
Vignesh <vigneshwersekar@gmail.com>
2018-10-04 02:51:49 +0530

    first

---
+++ src/main/java/com/stackroute/buzzup/moviescreeningservice/service/MovieScreeningService.java
@@ -0,0 +1,21 @@
+package com.stackroute.buzzup.moviescreeningservice.service;
+
+import java.util.List;
+
+import com.stackroute.buzzup.kafka.model.MovieSchedule;
+import com.stackroute.buzzup.kafka.model.Theatre;
+
+/*
+ * thisinterface contains methods which are to be implemented in serviceImpl class
+ */
+public interface MovieScreeningService {
+
+	
+	public MovieSchedule addMovieSchedule(MovieSchedule movieSchedule);
+
+	public List<MovieSchedule> getMoviesSchedule();
+	
+	public MovieSchedule updateMovieSchedule(String email, MovieSchedule updateMovie);
+	
+	
+}
